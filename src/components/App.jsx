2a6dfd0e158e0bd0df13a96f0c04592580ebdbc1
IoannisLykomitros <167572966+IoannisLykomitros@users.noreply.github.com>
2025-11-27 02:36:39 +0200

    added current word display with styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import Header from "./Header.jsx";
 import Status from "./Status.jsx";
 import LanguageList from "./LanguageList.jsx";
 
 const AssemblyEndgame = () => {
+
+  const [currentWord, setCurrentWord] = useState('react');
+
+  const letterElements = currentWord.split("").map((letter, index) => (
+    <span key={index} className="letter-box">{letter.toUpperCase()}</span>
+  ))
+
   return (
     <main>
       <Header />
       <Status />
       <LanguageList />
+      <section className="word-container">
+        {letterElements}
+      </section>
     </main>   
   )
 }
